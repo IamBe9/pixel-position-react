cfd6c2cb9ead29f58bfd5b043a6d991b307975aa
Daniel <u2_danhru_waw@technischoosl>
2025-05-12 13:38:45 +0200

    controllers completed

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -2,9 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\File;
 
 class RegisteredUserController extends Controller
 {
-    //
+
+    public function create()
+    {
+        return view('auth.register');
+    }
+
+
+    public function store(Request $request)
+    {
+        $userAttributes = request()->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required',  'email', 'max:255', 'unique:users,email'],
+            'password' => ['required', 'min:8', 'confirmed']
+        ]);
+
+        $employerAttributes = request()->validate([
+            'employer' => ['required'],
+            'logo' => ['required', File::types(['png', 'jpg', 'jpeg', 'webp'])],
+        ]);
+
+        $user = User::create($userAttributes);
+
+        $logoPath = $request->logo->store('logos');
+
+        $user->employer()->create([
+            'name' => $employerAttributes['employer'],
+            'logo' => $logoPath,
+        ]);
+
+        Auth::login($user);
+
+        return redirect('/');
+    }
+
 }
--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class SessionUserController extends Controller
+class SessionController extends Controller
 {
     //
 }
